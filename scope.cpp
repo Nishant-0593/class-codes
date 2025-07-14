6e8a86b3a4f6eb6127020be72fb00ed0146cadfb
Nishant-0593 <nishantpathania300@gmail.com>
2025-07-14 12:02:35 +0530

    Update scope.cpp

--- scope.cpp
+++ scope.cpp
@@ -1,15 +1,1 @@
-#include<iostream>
-using namespace std;
-int globals=11;
-void demo(){
-    int locals = 25;
-    cout<<"value of local in this is :"<<locals<<endl;
-    cout<<"value of globals varriable is :"<<globals<<endl;
 
-}
-
-int main(){
-    int locals=30;
-    cout<<"value of globals varriable :"<<globals<<endl;
-    
-}
