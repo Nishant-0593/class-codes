2cff6fbc04090dfa227aa9691d1d61f982bc817d
Nishant Pathania <nishantpathania300@gmail.com>
2025-07-14 12:11:42 +0530

    size explorer of each data type

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int marks;
-    cout<<"enter a marks:";
-    cin>>marks;
+int age = 21;  
+
+    short int smallNum = 32767;   
+    long int bigNum = 1000000;    
+    unsigned int points = 500;    
+    float pi = 3.14f;             
+    double gravity = 9.80665; 
     
+    cout<<"size of integer datatype is : "<<sizeof(age)<<endl;
+    cout<<"size of short int  datatype is : "<<sizeof(smallNum)<<endl;
+    cout<<"size of  long int datatype is : "<<sizeof(bigNum)<<endl;
+    cout<<"size of  unsigned int datatype is : "<<sizeof(points)<<endl;
+    cout<<"size of float datatype is : "<<sizeof(pi)<<endl;  
+    cout<<"size of double datatype is : "<<sizeof(gravity)<<endl;
+    return 0;
 }
