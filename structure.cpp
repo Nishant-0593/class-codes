264cc03dac232b659080685f4738282305275af1
Nishant Pathania <nishantpathania300@gmail.com>
2025-08-01 14:49:56 +0530

    structure .cpp

--- structure.cpp
+++ structure.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
 using namespace std;
-struct point{
-    int x;
-    int y;
+struct Point{
+    int x,y;
 };
-
-int main() {
-       point p;
-       p.x = 2;
-       p.y = 4;
-       cout<<p.x<<","<<p.y<<endl;
+int add(int x,int y){
+    return x + y;
+}
+int sub(int x,int y){
+    return x - y;
+}
+int mul(int x,int y){
+    return x * y;
+}
+float divi(int x,int y){
+    return x / y;
+};
+int main(){
+    Point p;
+    p.x = 3;
+    p.y = 4;
+    cout << "Addition:" << add(p.x,p.y) << endl;
+    cout << "Difference:" << sub(p.x,p.y) << endl;
+    cout << "Multiplication:" << mul(p.x,p.y) << endl;
+    cout << "Division:" << divi(p.x,p.y) << endl;
     return 0;
 }
